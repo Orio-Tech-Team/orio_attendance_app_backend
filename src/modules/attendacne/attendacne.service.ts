60cedfaea4af14ae48d5cd57bb4880c39ed839f4
Muhammad Ismail <itsmuhammadismail@gmail.com>
2022-12-08 13:04:55 +0500

    new

--- src/modules/attendacne/attendacne.service.ts
+++ src/modules/attendacne/attendacne.service.ts
@@ -25,10 +25,7 @@ export class AttendacneService {
     );
     const attendanceTime = GetDate.currentTime();
     let attendanceType = "Present";
-    if (
-      employee.shift.start_time.getTime() <
-      new Date(attendanceTime).getTime() - 10 * 60000
-    ) {
+    if (employee.shift.start_time.getTime() < new Date().getTime() - 600000) {
       attendanceType = "Late";
     } else {
       attendanceType = "Present";
